224806196f64d2102068f9feae6272f966fc4320
alejandrocamilo <alejandrocamilojavier5@gmail.com>
2025-06-01 12:35:01 -0400

    Database Creation

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import {PORT} from './config/env.js'
 import authRouter from './routes/auth.routes.js'
 import userRouter from './routes/user.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -17,8 +18,10 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(PORT, ()=> {
+app.listen(PORT, async () => {
     console.log(`Subscription Tracker API on http://localhost:${PORT}`);
+
+    await connectToDatabase();
 });
 
 export default app;
