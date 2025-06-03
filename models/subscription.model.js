a933b3e1e7830f089d721336a3d77b303879ccdc
alejandrocamilo <alejandrocamilojavier5@gmail.com>
2025-06-03 17:10:08 -0400

    adding middlewares

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -76,6 +76,7 @@ subscriptionSchema.pre('save', function (next) {
 
         this.renewalDate = new Date(this.startDate);
         this.renewalDate.setDate(this.renewalDate.getDate() + renewalPeriods[this.frequency]);
+
     }
 
     // Auto-update the status if renewal date has passed
