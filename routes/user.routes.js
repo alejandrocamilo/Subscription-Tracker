922067daa722f858d4cf76d8f8b502537d09c7d6
alejandrocamilo <alejandrocamilojavier5@gmail.com>
2025-06-04 19:57:14 -0400

    adding controllers to their routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,13 @@
 import {Router} from 'express'
+import {getUser, getUsers} from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send({title:'GET all users'}));
-userRouter.get('/:id', (req, res) => res.send({title:'GET user details'}))
+userRouter.get('/', getUsers);
+userRouter.get('/:id', getUser)
 userRouter.post('/', (req, res) => res.send({title:'Create new user'}));
 userRouter.put('/:id', (req, res) => res.send({title:'Update user'}))
-userRouter.delete('/:ID', (req, res) => res.send({title: 'Delete user'}))
+userRouter.delete('/:id', (req, res) => res.send({title: 'Delete user'}))
 
 export default userRouter;
 
